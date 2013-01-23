7d939647b38c8bcbbf419559fe700068983ce9a4
student <student@student-VirtualBox>
2013-01-23 16:39:48 +0100

    drobne poprawki

--- Business/src/main/java/Shop/Office/Web/PeselValidator.java
+++ Business/src/main/java/Shop/Office/Web/PeselValidator.java
@@ -7,7 +7,7 @@ import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
-@FacesValidator("pinValidator")
+@FacesValidator("PeselValidator")
 public class PeselValidator implements Validator {
 
 	@Override
